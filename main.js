47dce0866313ec28b14c59b429312ee6b018058f
AnnJose24 <ann.jose@litmus7.com>
2024-09-06 14:05:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import { ExperienceManager } from "./experience.js";
+import { EducationManager } from "./education.js";
+import { ContactManager } from "./contact.js";
+
+new ExperienceManager();
+new EducationManager();
+new ContactManager();
+
+
+
+
+
+
+
+
+
